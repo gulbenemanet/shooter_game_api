ca167fdefea8debcc3e4fdec2e280d4eee8ed8a6
DESKTOP-GOC40BQ\roseme <gulbenemanet@gmail.com>
2024-06-07 22:11:01 +0300

    joinroom düzenlendi

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -141,10 +141,11 @@ const updateOneRoom = async (req, res) => {
 
 const joinRoom = async (req, res) => {
   try {
-    // console.log(req.params);
-    const room = await Room.findById(req.body.roomId);
-    const { userId } = req.body.userId;
-    const user = await Gamer.findById( userId );
+    const { roomId, userId } = req.body;
+    // console.log(roomId);
+    const room = await Room.findById(roomId);
+
+    const user = await Gamer.findById(userId);
     if (!room) {
       return res.status(404).json({ message: 'Oda bulunamadı' });
     }
@@ -156,11 +157,11 @@ const joinRoom = async (req, res) => {
       room.count = room.count + 1;
       await room.save();
     }
-    io.to(req.params.roomId).emit('userJoined', { userId: user._id, roomId: req.params.roomId });
+    // io.to(req.body.roomId).emit('userJoined', { userId: user._id, roomId: req.params.roomId });
     res.json({ message: 'Odaya katıldınız' });
   } catch (err) {
-    res.status(500).json({ message: 'Sunucu hatası: ' + err });
-  }
+    res.status(500).json({ message: 'Sunucu hatası: ' + err });
+  }
 };
 
 const leaveRoom = async (req, res) => {
